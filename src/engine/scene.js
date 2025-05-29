35910ce984458db8352c08ef89189adef1629a96
unknown <denn522.guru@gmail.com>
2025-05-29 11:08:03 +0200

    add engine JS files in src

---
+++ src/engine/scene.js
@@ -0,0 +1,26 @@
+import { Keyboard } from "./keyboard.js";
+import { Sound } from "./sound.js";
+
+export class Scene {
+  constructor(game, view) {
+    this.game = game;
+    this.view = view;
+    this.keyboard = new Keyboard();
+    this.sound = new Sound();
+  }
+
+  start() { }
+  stop() { }
+
+  on_start() {
+    this.keyboard.reset();
+    this.start();
+    return this.view.show();
+  }
+
+  on_stop() {
+    this.stop();
+    this.keyboard.reset();
+    return this.view.hide();
+  }
+}
